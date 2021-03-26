3df6a8e5c530ab0380e7f20263bdd2cb168b3cf8
RejzDev <aniraze86@gmail.com>
2021-03-26 19:51:33 +0200

    DayOfWeak

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -9,11 +9,34 @@ function indexAction($smarty){
        return;
    }
    //$cityName = 'London';
+    
+   
    $rsWeather = getWeatherDate($nameCity);
+  $date = $rsWeather->list[0]->dt_txt;
+  //foreach ($rsWeather as $item){
+  //    $gg = $item['name'];
+  //}
+    
+    $dates = date("l", strtotime($date));
+    
+    // Перевод
+   // $days = [
+   //     'Воскресенье', 'Понедельник', 'Вторник', 'Среда',
+   //     'Четверг', 'Пятница', 'Суббота'
+   // ];
+   //
+   // foreach($dates as $d){
+   //     $dayOfWeek = array(
+   //         'date' => $d,
+   //         'dayOfWeek' => date("w", strtotime($d) )
+   //     );
+   // }
+  //$date = date('l', $date);
+ // d($dates);
    
    $smarty->assign('pageTitle', $nameCity);
    $smarty->assign('rsWeather', $rsWeather);
-   
+   //d($gg);
   
    loadTemplate($smarty, 'header');
    loadTemplate($smarty, 'search');
